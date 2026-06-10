1f0cb36b4c9c9c2831a64420d8085e3890aebe74
riheun <riheun2@naver.com>
2026-06-10 21:11:34 +0900

    커밋메세지

--- src/app/(main)/login/page.jsx
+++ src/app/(main)/login/page.jsx
@@ -1,137 +1,166 @@
-'use client';
-import { useEffect, useState} from 'react';
-import baseApi from '@/common/api/baseApi';
+"use client";
+import { useEffect, useState } from "react";
+import baseApi from "@/common/api/baseApi";
 
-import s from './login.module.css';
+import s from "./login.module.css";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
+  const [loginInfo, setLoginInfo] = useState();
 
-    const [logigInfo, setLoginInfo] = useState();
+  const goLogin = async () => {
+    try {
+      const res = await baseApi.post("/api/v1/employees/login", loginInfo);
 
-    const goLogin = async () => {
-        const res = await baseApi.post('/api/v1/employees/login', {...logigInfo});
+      const token = res.data.data.accessToken;
+      if (token) {
         localStorage.setItem("accessToken", res.data.data.accessToken);
+        router.push("/user");
+      } else {
+        alert("로그인 실패");
+      }
+    } catch (e) {
+      console.error("졸려요", e);
+    } finally {
     }
+  };
 
-    useEffect(() => {
-        const getEmployees = async () => {
-            const token = localStorage.getItem('accessToken');
-            const res = await baseApi.get('/api/v1/employees', {
-                headers: {
-                    'Authorization':
-                    'Bearer ${token}'
-                }
-            });
-            console.log(res);
-        }
-        getEmployees();
-    }, []);
+  useEffect(() => {
+    const getEmployees = async () => {
+      const token = localStorage.getItem("accessToken");
+      const res = await baseApi.get("/api/v1/employees", {
+        headers: {
+          Authorization: "Bearer ${token}",
+        },
+      });
+      console.log(res);
+    };
+    getEmployees();
+  }, []);
 
-    return (
-        <div className={s.container}>
-            <div className={s.header}>
-                <div className={s.logo}>
-                    <img src="/bag.png" alt="" />
-                    <h3>인사관리시스템</h3>
-                </div>
-                <div className={s.header_list}>
-                    <ul className={s.header_menu_list}>
-                        <li>인사관리</li>
-                        <li>근태관리</li>
-                        <li>급여관리</li>
-                        <li>일용직관리</li>
-                    </ul>
-                </div>
-            </div>
-
-            <div className={s.ad}>
-                <div className={s.content}>
-                    <div className={s.content_auth}>
-                        <img src="/check.png" alt="" />
-                        <h3>Enterprise HR Solution</h3>
-                    </div>
-                    <div className={s.content_title}>
-                        <h2>스마트한 인사관리의</h2>
-                        <h3>새로운 기준</h3>
-                        <span>직원 채용부터 급여, 근태까지 <br />하나의 플랫폼으로 관리하세요.</span>
-                    </div>
-                    <div className={s.content_num}>
-                        <ul className={s.num_list}>
-                            <li>
-                                <h3>2,400+</h3>
-                                <p>기업 도입</p>
-                            </li>
-                            <li>
-                                <h3>98%</h3>
-                                <p>고객 만족도</p>
-                            </li>
-                            <li>
-                                <h3>15년</h3>
-                                <p>서비스 운영</p>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className={s.table}>
-                    <div className={s.table_list}>
-                        <ul className={s.table_menu_list}>
-                            <li>
-                                <div className={s.img_box}>
-                                    <img src="/users.png" alt="" />
-                                </div>
-                                <h3>인사관리</h3>
-                                <span>조직도, 인사발령, 직원 정보 통합 관리</span>
-                            </li>
-                            <li>
-                                <div className={s.img_box}>
-                                    <img src="/moneym.png" alt="" />
-                                </div>
-                                <h3>급여관리</h3>
-                                <span>자동 급여 계산, 세금 신고, 명세서 발송</span>
-                            </li>
-                            <li>
-                                <div className={s.img_box}>
-                                    <img src="/users.png" alt="" />
-                                </div>
-                                <h3>근태관리</h3>
-                                <span>출퇴근, 초과근무 실시간 모니터링</span>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-             
-
-            <div className={s.login}>
-                <div className={s.login_title}>
-                    <h3>로그인</h3>
-                    <span>계정에 로그인하여 업무를 시작하세요</span>
-                </div>
-                <div className={s.input_box}>
-                    <label htmlFor="email">이메일</label>
-                    <input type="text" className={s.email} placeholder='이메일 주소를 입력하세요' onChange={(e) => setLoginInfo(prev => ({...prev, email: e.target.value}))} />
-                    <label htmlFor="password">비밀번호</label>
-                    <input type="password" className={s.password} placeholder='비밀번호를 입력하세요' onChange={(e) => setLoginInfo(prev => ({...prev, password: e.target.value}))} />
-                    <input type="checkbox" className={s.check} /><span>로그인 상태 유지</span>
-                    <strong>비밀번호 찾기</strong>
-                </div>
-                <button className={s.login_btn} onClick={() => goLogin()}>
-                    <img src="/login.png" alt="" />
-                    <h3>로그인</h3>
-                </button>
-                <div className={s.or}>
-                    <div className={s.line1}>선1</div>
-                    <h3>또는</h3>
-                    <div className={s.line2}>선2</div>
-                </div>
-                <div className={s.kakao}>
-                    <img src="/kakao.png" alt="" />
-                </div>
-                <div className={s.ask}>
-                    <span>계정이 없으신가요?</span>
-                    <strong>회원가입 신청</strong>
-                </div>
-            </div>
+  return (
+    <div className={s.container}>
+      <div className={s.header}>
+        <div className={s.logo}>
+          <img src="/bag.png" alt="" />
+          <h3>인사관리시스템</h3>
         </div>
-    )
+        <div className={s.header_list}>
+          <ul className={s.header_menu_list}>
+            <li>인사관리</li>
+            <li>근태관리</li>
+            <li>급여관리</li>
+            <li>일용직관리</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className={s.ad}>
+        <div className={s.content}>
+          <div className={s.content_auth}>
+            <img src="/check.png" alt="" />
+            <h3>Enterprise HR Solution</h3>
+          </div>
+          <div className={s.content_title}>
+            <h2>스마트한 인사관리의</h2>
+            <h3>새로운 기준</h3>
+            <span>
+              직원 채용부터 급여, 근태까지 <br />
+              하나의 플랫폼으로 관리하세요.
+            </span>
+          </div>
+          <div className={s.content_num}>
+            <ul className={s.num_list}>
+              <li>
+                <h3>2,400+</h3>
+                <p>기업 도입</p>
+              </li>
+              <li>
+                <h3>98%</h3>
+                <p>고객 만족도</p>
+              </li>
+              <li>
+                <h3>15년</h3>
+                <p>서비스 운영</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={s.table}>
+          <div className={s.table_list}>
+            <ul className={s.table_menu_list}>
+              <li>
+                <div className={s.img_box}>
+                  <img src="/users.png" alt="" />
+                </div>
+                <h3>인사관리</h3>
+                <span>조직도, 인사발령, 직원 정보 통합 관리</span>
+              </li>
+              <li>
+                <div className={s.img_box}>
+                  <img src="/moneym.png" alt="" />
+                </div>
+                <h3>급여관리</h3>
+                <span>자동 급여 계산, 세금 신고, 명세서 발송</span>
+              </li>
+              <li>
+                <div className={s.img_box}>
+                  <img src="/users.png" alt="" />
+                </div>
+                <h3>근태관리</h3>
+                <span>출퇴근, 초과근무 실시간 모니터링</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className={s.login}>
+        <div className={s.login_title}>
+          <h3>로그인</h3>
+          <span>계정에 로그인하여 업무를 시작하세요</span>
+        </div>
+        <div className={s.input_box}>
+          <label htmlFor="email">이메일</label>
+          <input
+            type="text"
+            className={s.email}
+            placeholder="이메일 주소를 입력하세요"
+            onChange={(e) =>
+              setLoginInfo((prev) => ({ ...prev, email: e.target.value }))
+            }
+          />
+          <label htmlFor="password">비밀번호</label>
+          <input
+            type="password"
+            className={s.password}
+            placeholder="비밀번호를 입력하세요"
+            onChange={(e) =>
+              setLoginInfo((prev) => ({ ...prev, password: e.target.value }))
+            }
+          />
+          <input type="checkbox" className={s.check} />
+          <span>로그인 상태 유지</span>
+          <strong>비밀번호 찾기</strong>
+        </div>
+        <button className={s.login_btn} onClick={() => goLogin()}>
+          <img src="/login.png" alt="" />
+          <h3>로그인</h3>
+        </button>
+        <div className={s.or}>
+          <div className={s.line1}>선1</div>
+          <h3>또는</h3>
+          <div className={s.line2}>선2</div>
+        </div>
+        <div className={s.kakao}>
+          <img src="/kakao.png" alt="" />
+        </div>
+        <div className={s.ask}>
+          <span>계정이 없으신가요?</span>
+          <strong>회원가입 신청</strong>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/(main)/user/page.jsx
+++ src/app/(main)/user/page.jsx
@@ -1,117 +1,131 @@
-'use client';
-import baseApi from '@/common/api/baseApi';
-import style from './test.module.css';
-import { useState, useEffect } from 'react';
-import Nav from '@/component/nav';
-import Sub from '@/component/sub';
+"use client";
+import baseApi from "@/common/api/baseApi";
+import style from "./test.module.css";
+import { useState, useEffect } from "react";
+import Nav from "@/component/nav";
+import Sub from "@/component/sub";
 
-export default function page({path, buttonName, onClick, placeholder, onChange}) {
+export default function page() {
+  const [employees, setEmployees] = useState([]);
+  const [keyword, setKeyword] = useState();
 
-    useEffect(() => {
-        const getApi = async () => {
-            const res = await baseApi.get('/api/v1/employees');
-        }
-        getApi();
-    })
+  const getEmployees = async () => {
+    const token = localStorage.getItem("accessToken");
+    const res = await baseApi.get("/api/v1/employees", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        keyword: keyword || "",
+        page: 1,
+      },
+    });
+    console.log(res.data.data);
+    setEmployees(res.data.data);
+  };
 
-    const [keyword, setKeyword] = useState();
+  useEffect(() => {
+    getEmployees();
+  }, []);
 
-
-    const getEmployees = async () => {
-        const res = await baseApi.get('/api/v1/employees', {
-            params: {
-                keyword: keyword
-            }
-        });
-        console.log(res.data.data);
-    }
-
-
-    return (
-        <div className={style.container}> 
-            <Nav/>
-            <Sub/>
-            <section className={style.hi}>
-                <div className={style.hi_mini}>
-                    <img src='/home.png' />
-                    <img src='/right.png' />
-                    <h4>인사관리</h4>
-                    <img src='/right.png' />
-                    <h4>인사정보</h4>
-                    <img src='/right.png' />
-                    <h5>인사정보등록</h5>
-                </div>
-                <div className={style.hi_header}>
-                    <h3>인사정보등록</h3>
-                    <span>직원의 인사정보를 등록하고 관리합니다.</span>
-                    <div className={style.down}>
-                        <img src='/download.png' />
-                        <h4>PDF 다운로드</h4>
-                    </div>
-                    <div className={style.regist}>
-                        <img src='/plus.png' />
-                        <h4>신규등록</h4>
-                    </div>
-                </div>
-                <div className={style.input_box}>
-                    <div className={style.input_title}>
-                        <img src='/search.png' />
-                        <h4>검색조건</h4>
-                    </div>
-                    <div className={style.input_content}>
-                        <label>사원번호</label>
-                        <input placeholder='전체'
-                            onChange={(e) => setKeyword(e.target.value)}
-                            className={style.number}
-                        />
-                        <label>부서</label>
-                        <select className={style.depart}>
-                            <option>전체</option>
-                            <option>경영지원팀</option>
-                            <option>개발팀</option>
-                            <option>영업팀</option>
-                            <option>인사팀</option>
-                        </select>
-                        <label>직급</label>
-                        <select className={style.rank}>
-                            <option>전체</option>
-                            <option>팀장</option>
-                            <option>과장</option>
-                            <option>대리</option>
-                            <option>사원</option>
-                        </select>
-                        <label>재직상태</label>
-                        <select className={style.ing}>
-                            <option>재직중</option>
-                            <option>휴직중</option>
-                        </select>
-                        <button className={style.search_btn}
-                            onClick={() => getEmployees()}
-                        >
-                            <img src='/search2.png' />
-                            <h3>조회</h3>
-                        </button>
-                        <button className={style.back}>
-                            <img src='/back.png' />
-                            <h3>초기화</h3>
-                        </button>
-                    </div>
-                    <div className={style.data}>
-                        <ul className={style.data_menu_list}>
-                            <li>No</li>
-                            <li>사원번호</li>
-                            <li>성명</li>
-                            <li>부서</li>
-                            <li>직급</li>
-                            <li>입사일</li>
-                            <li>연락처</li>
-                            <li>이메일</li>
-                            <li>재직상태</li>
-                            <li>관리</li>
-                        </ul>
-                    </div>
-                </div>
-            </section>
+  return (
+    <div className={style.container}>
+      <Nav />
+      <Sub />
+      <section className={style.hi}>
+        <div className={style.hi_mini}>
+          <img src="/home.png" />
+          <img src="/right.png" />
+          <h4>인사관리</h4>
+          <img src="/right.png" />
+          <h4>인사정보</h4>
+          <img src="/right.png" />
+          <h5>인사정보등록</h5>
         </div>
-    )
+        <div className={style.hi_header}>
+          <h3>인사정보등록</h3>
+          <span>직원의 인사정보를 등록하고 관리합니다.</span>
+          <div className={style.down}>
+            <img src="/download.png" />
+            <h4>PDF 다운로드</h4>
+          </div>
+          <div className={style.regist}>
+            <img src="/plus.png" />
+            <h4>신규등록</h4>
+          </div>
+        </div>
+        <div className={style.input_box}>
+          <div className={style.input_title}>
+            <img src="/search.png" />
+            <h4>검색조건</h4>
+          </div>
+          <div className={style.input_content}>
+            <label>사원번호</label>
+            <input
+              placeholder="전체"
+              onChange={(e) => setKeyword(e.target.value)}
+              className={style.number}
+            />
+            <label>부서</label>
+            <select className={style.depart}>
+              <option>전체</option>
+              <option>경영지원팀</option>
+              <option>개발팀</option>
+              <option>영업팀</option>
+              <option>인사팀</option>
+            </select>
+            <label>직급</label>
+            <select className={style.rank}>
+              <option>전체</option>
+              <option>팀장</option>
+              <option>과장</option>
+              <option>대리</option>
+              <option>사원</option>
+            </select>
+            <label>재직상태</label>
+            <select className={style.ing}>
+              <option>재직중</option>
+              <option>휴직중</option>
+            </select>
+            <button className={style.search_btn} onClick={() => getEmployees()}>
+              <img src="/search2.png" />
+              <h3>조회</h3>
+            </button>
+            <button className={style.back}>
+              <img src="/back.png" />
+              <h3>초기화</h3>
+            </button>
+          </div>
+          <div className={style.data}>
+            <ul className={style.data_menu_list}>
+              <li>No</li>
+              <li>사원번호</li>
+              <li>성명</li>
+              <li>부서</li>
+              <li>직급</li>
+              <li>입사일</li>
+              <li>연락처</li>
+              <li>이메일</li>
+              <li>재직상태</li>
+              <li>관리</li>
+            </ul>
+            {employees.map((item, index) => (
+              <ul className={style.maindbList} key={index}>
+                <li>{index + 1}</li>
+                <li>{item.employeeNo}</li>
+                <li>{item.name}</li>
+                <li>{item.data_dept}</li>
+                <li>{item.data_level}</li>
+                <li>{item.data_date}</li>
+                <li>{item.data_tel}</li>
+                <li style={{ textAlign: "left" }}>{item.data_email}</li>
+                <li>{item.data_status}</li>
+                <li>{item.data_manage}</li>
+              </ul>
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
